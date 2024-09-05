9c5e57d34cf2270fd4f1ba1b25127c1d5d197edd
Özlem Akman <ozlemakman16@gmail.com>
2024-09-06 00:32:49 +0300

    Full Join ,İnner join, Left join, Right join

--- queries.sql
+++ queries.sql
@@ -88,4 +88,22 @@ select country_id, COUNT(*) as sehirsayisi FROM city
 GROUP BY country_id 
 ORDER BY sehirsayisi DESC 
 LIMIT 1;
+--29.city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT * From city
+INNER JOIN country ON city.country_id = country.country_id;
+--30.customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+Select customer.first_name, customer.last_name From customer 
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
+--31.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT customer.first_name, customer.last_name FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id;
+--32.city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+select * From city 
+LEFT JOIN country ON city.country_id = country.country_id;
+--33.customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+select customer.first_name, customer.last_name FROM payment 
+RIGHT JOIN customer ON customer.customer_id = payment.customer_id
+--34.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+select customer.first_name, customer.last_name from customer 
+FUll JOIN rental ON customer.customer_id = rental.customer_id;
 
